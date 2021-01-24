691594b12257dc48136d2d4c9b8977a2c10bac02
Patrick Spears <spearspa@gmail.com>
2021-01-24 16:39:37 -0500

    build: added filler content

--- frontend/src/views/Campaigns.vue
+++ frontend/src/views/Campaigns.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="campaigns">
-    <h1>This is an about page</h1>
+    <h1>A history of campaigns we've played</h1>
+    <p>This page will contain a collection of campaigns we've designed & played in.</p>
+    <p>It might even be a whole-ass own app.</p>
   </div>
 </template>
--- frontend/src/views/Stories.vue
+++ frontend/src/views/Stories.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="stories">
-    <h1>This is an about page</h1>
+    <h1>This is for one-shots.</h1>
+    <p>Like just a collection of one-shot games or adventures or whatever,
+      not everything is an adventure.</p>
   </div>
 </template>
