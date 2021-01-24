cd0f38a5264ed674702f9c5248a41f6e1879288a
Patrick Spears <spearspa@gmail.com>
2021-01-24 14:34:52 -0500

    build: add new routes to Header

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,6 +1,17 @@
 <template>
-  <div id="Header">
-    This is the header text.
+  <div id="header-container">
+    <header>
+      <nav id="navbar-menu">
+        <div>
+          <h1>the labyrinth</h1>
+        </div>
+        <router-link to="/">Homes</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link to="/games">Games</router-link>
+        <router-link to="/campaigns">Campaigns</router-link>
+        <router-link to="/stories">Stories</router-link>
+      </nav>
+    </header>
   </div>
 </template>
 
