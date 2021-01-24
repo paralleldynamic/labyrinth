875ac57a067b5ec7ccc89911b06b4d3a202e6ba3
Patrick Spears <spearspa@gmail.com>
2021-01-24 17:04:36 -0500

    build: add stub data for cards to API

--- backend/labyrinth.py
+++ backend/labyrinth.py
@@ -1,7 +1,28 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 
-#configuration
+
+# stub data for testing
+
+game_cards = [
+    {
+        'title': 'Eclipse Phase',
+        'website': 'https://eclipsephase.com/',
+        'logo_img_src': 'https://eclipsephase.com/sites/all/themes/eclipse/logo.png',
+        'logo_img_alt': 'eclipse phase logo',
+        'tagline': 'a game of transhuman horror',
+    },
+    {
+        'title': 'Genesys',
+        'website': 'https://www.fantasyflightgames.com/en/products/genesys/',
+        'logo_img_src': 'https://images-cdn.fantasyflightgames.com/filer_public/70/67/7067c9e1-60bb-49ae-9b11-7d6e7926961d/gns01_anc_slider.jpg',
+        'logo_img_alt': 'genesys logo',
+        'tagline': 'a universal RPG based around winning narrative control',
+    },
+]
+
+
+# configuration
 DEBUG = True
 
 # instantiate the app
