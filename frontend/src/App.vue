c9e1b7614816a012cbff509ddb5986ddb5cf2326
Patrick Spears <spearspa@gmail.com>
2021-01-24 15:07:16 -0500

    build: created Footer component & added to app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,14 +3,17 @@
   <div id="page-content">
     <router-view />
   </div>
+  <Footer />
 </template>
 
 <script>
 import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
 
 export default {
   components: {
     Header,
+    Footer,
   },
 };
 </script>
---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="footer-container">
+    <footer>
+      <div id="search-bar">
+        <input id="search-input" type="search" name="search-key" placeholder="enter search term">
+        <button id="search-submit">search</button>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
+
+<style scoped>
+#footer-container {
+  background-color: #E5E7E9;
+  height: var(--footer-container-height);
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  box-shadow: 0 -1px 2px rgba(0,0,0,0.19);
+}
+
+#search-bar {
+  vertical-align: middle;
+  float: right;
+  padding-right: 1em;
+  padding-top: 0.5em;
+  padding-bottom: 0.5em;
+}
+</style>
