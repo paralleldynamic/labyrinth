757af3ab6a7619e5e2346066c0e2c600f8aa4590
Patrick Spears <spearspa@gmail.com>
2021-02-13 10:14:34 -0500

    fix: blueprints can't have the same name

--- backend/core/game/__init__.py
+++ backend/core/game/__init__.py
@@ -3,7 +3,7 @@ from flask_restx import Api
 
 from core.game.controller import ns as game_ns
 
-blueprint = Blueprint('api', __name__)
+blueprint = Blueprint('game_api', __name__)
 api = Api(blueprint)
 
 api.add_namespace(game_ns, path='/game')
