26e3e6f19281a5be60216aa18f7cfe7fb2b8d02c
Patrick Spears <spearspa@gmail.com>
2021-02-14 16:50:45 -0500

    build: create standard page layout for standard pages

---
+++ frontend/src/components/Page.vue
@@ -0,0 +1,29 @@
+<template>
+  <HeaderContainer />
+  <div id="page-content">
+    <router-view />
+  </div>
+  <FooterContainer />
+</template>
+
+<script>
+import HeaderContainer from '@/components/HeaderContainer.vue';
+import FooterContainer from '@/components/FooterContainer.vue';
+
+export default {
+  components: {
+    HeaderContainer,
+    FooterContainer,
+  },
+};
+</script>
+
+<style scoped>
+#page-content {
+  margin-top: var(--header-container-height);
+  margin-bottom: var(--footer-container-height);
+  height: 100%;
+  display: flex;
+  align-items: stretch;
+}
+</style>
