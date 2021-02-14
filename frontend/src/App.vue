bacdcae961704a67ab183020b27ede65aaaa4ffd
Patrick Spears <spearspa@gmail.com>
2021-02-14 16:50:11 -0500

    build: simply app template so multiple layout components can be used

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,6 @@
 <template>
-  <HeaderContainer />
-  <div id="page-content">
-    <router-view />
-  </div>
-  <FooterContainer />
+  <router-view />
 </template>
-
-<script>
-import HeaderContainer from '@/components/HeaderContainer.vue';
-import FooterContainer from '@/components/FooterContainer.vue';
-
-export default {
-  components: {
-    HeaderContainer,
-    FooterContainer,
-  },
-};
-</script>
 
 <style scoped>
 #page-content {
