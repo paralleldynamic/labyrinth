a2f42a021e570d0c0550b9424638d04aabdad303
Patrick Spears <spearspa@gmail.com>
2021-02-28 21:28:44 -0500

    style: change charge max size

--- frontend/src/components/ContentCards.vue
+++ frontend/src/components/ContentCards.vue
@@ -57,7 +57,7 @@ export default {
   box-shadow: 0 5px 10px #191919;
   border-radius: 3px;
   box-sizing: border-box;
-  max-width: 25%;
+  max-width: 25vw;
   padding: 0.25em;
   margin: 0.5em;
   background-color: #FBFBFF;
