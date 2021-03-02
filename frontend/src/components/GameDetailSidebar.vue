f5db920f2db53fa00aa7e2a796bc2fcef6d02aff
Patrick Spears <spearspa@gmail.com>
2021-03-01 23:25:47 -0500

    refactor: game detail sidebar to use slots, extended data model prep

--- frontend/src/components/GameDetailSidebar.vue
+++ frontend/src/components/GameDetailSidebar.vue
@@ -1,19 +1,50 @@
 <template>
   <sidebar :show="show" @toggleSidebar="toggleSidebar">
-    <div class="game-detail-sidebar">
-      <div class="sidebar-header">
-        <img id="sidebar-image" v-bind:src="game.logo_img_src"
+    <template v-slot:sidebar-header>
+      <img id="game-logo"
+            v-bind:src="game.logo_img_src"
             v-bind:alt="game.logo_img_alt"
             v-bind:style="game.logo_img_style">
-        <h1 id="sidebar-title">{{ game.title }}</h1>
+      <p>{{ game.tagline }}</p>
+    </template>
+    <template v-slot:sidebar-content>
+      <div class="sidebar-content-container">
+        <h1>{{ game.title }}</h1>
+        <div id="publisher-website">
+          <p>Publisher Website:
+            <a v-bind:href="game.publisher_website" target="_blank">
+              {{ game.publisher ? game.publisher : "link to publisher" }}
+            </a>
+          </p>
+        </div>
+        <div id="extended-description">
+          <p> {{ game.extended_description ? game.extended_description :
+            `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum.` }}
+          </p>
+        </div>
+        <div id="scales">
+        </div>
+        <div id="tags">
+          <p>universal</p>
+          <p>narrative</p>
+          <p>sandbox</p>
+          <p>cinematic</p>
+        </div>
       </div>
-      <div class="sidebar-content">
-        <p id="sidebar-description">{{ game.tagline }}</p>
+    </template>
+    <template v-slot:sidebar-footer>
+      <div class="sidebar-controls">
+        <button @click="toggleEdit" v-if="true"><ph-pencil-line :size="24" /></button>
+        <button @click="submitUpdate" v-if="true"><ph-floppy-disk :size="24" /></button>
+        <button @click="submitDelete" v-if="true"><ph-trash :size="24" /></button>
+        <button @click="toggleSidebar(game)" v-if="true"><ph-x-circle :size="24" /></button>
       </div>
-      <div class="sidebar-footer">
-        <button @click="submitDelete" v-if="false">Delete This Game</button>
-      </div>
-    </div>
+    </template>
   </sidebar>
 </template>
 
@@ -36,48 +67,54 @@ export default {
   methods: {
     ...mapActions(['deleteGame']),
     toggleSidebar(game) {
+      console.log(game.id);
       this.$emit('toggleSidebar', game);
     },
     async submitDelete() {
-      const { id } = this.game;
-      const { accessToken } = this.$store.state.auth;
-      await this.deleteGame({ id, accessToken })
-        .then(this.toggleSidebar(this.game))
-        .catch((error) => error);
+      // const { id } = this.game;
+      // const { accessToken } = this.$store.state.auth;
+      // await this.deleteGame({ id, accessToken })
+      //   .then(this.toggleSidebar(this.game))
+      //   .catch((error) => error);
     },
   },
 };
 </script>
 
 <style scoped>
-.game-detail-sidebar {
-  width: 100%;
-  margin: 1em;
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  grid-template-rows: repeat(10, 1fr);
+#extended-description > p {
+  margin: 0;
+  padding: 0;
 }
 
-.sidebar-header {
-  grid-area: 1 / 1 / 2 / 6;
-}
-
-.sidebar-content {
-  grid-area: 2 / 1 / 9 / 6;
-}
-
-.sidebar-footer {
-  grid-area: 9 / 1 / 11 / 6;
-}
-
-.sidebar-content > button {
-  width: 80%;
-  justify-self: center;
-  text-align: center;
-}
-
-#sidebar-image {
+#game-logo {
   max-width: 100%;
   max-height: 100%;
+}
+
+#extended-description {
+  padding: 0.5em;
+  margin: 0.5em;
+  font-size: smaller;
+  text-align: justify;
+  text-justify: newspaper;
+}
+
+ #tags {
+   display: flex;
+ }
+
+ .sidebar-controls > button {
+   margin: 0 1em 0 1em;
+   background-color: transparent;
+   border: none;
+ }
+
+.sidebar-controls {
+  height: 100%;
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: center;
+  align-items: center;
 }
 </style>
--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -1,11 +1,17 @@
 <template>
-  <transition name="slide">
-    <div @click.stop
-         v-if="show"
-         class="sidebar">
-      <slot />
+  <div class="sidebar-wrapper" v-if="show">
+    <div class="sidebar">
+      <div class="sidebar-header">
+        <slot name="sidebar-header" />
+      </div>
+      <div class="sidebar-content">
+        <slot name="sidebar-content" />
+      </div>
+      <div class="sidebar-footer">
+        <slot name="sidebar-footer" />
+      </div>
     </div>
-  </transition>
+  </div>
 </template>
 
 <script>
@@ -26,12 +32,39 @@ export default {
 </script>
 
 <style scoped>
-.sidebar {
+.sidebar-wrapper {
   background-color: #FBFBFF;
   box-shadow: 0 2px 4px #191919;
   display: flex;
   margin-top: 0.5em;
   height: 97.5%;
   width: 50vw;
+}
+
+.sidebar {
+  width: 100%;
+  margin: 1em;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: repeat(10, 1fr);
+  text-align: center;
+}
+
+.sidebar-header {
+  grid-area: 1 / 1 / 2 / 6;
+}
+
+.sidebar-content {
+  grid-area: 2 / 1 / 10 / 6;
+}
+
+.sidebar-footer {
+  grid-area: 10 / 1 / 11 / 6;
+}
+
+.sidebar-content > button {
+  width: 80%;
+  justify-self: center;
+  text-align: center;
 }
 </style>
