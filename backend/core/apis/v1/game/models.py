e298fdd563d0ef54c9c6edd8b9596180d3d3e1bf
Patrick Spears <spearspa@gmail.com>
2021-03-03 22:16:39 -0500

    build: get or create publisher method

--- backend/core/apis/v1/game/models.py
+++ backend/core/apis/v1/game/models.py
@@ -11,7 +11,6 @@ class Game(PkModel):
     website = Column(db.String(255))
     description = Column(db.Text())
     publisher_id = reference_col("publishers", nullable=False)
-    publisher = relationship("Publisher", backref="games")
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -27,11 +26,50 @@ class Publisher(PkModel):
     """Publishers of games."""
     __tablename__ = "publishers"
 
-    name = Column(db.String(255))
+    name = Column(db.String(255), nullable=False)
     website = Column(db.String(255))
+
+    @classmethod
+    def get_by_name(cls, search):
+        return cls.query.filter(cls.name.ilike(search)).first()
+
+    @classmethod
+    def get_or_create(cls, publisher):
+        id  = publisher.get('id')
+        name = publisher.get('name')
+        print(id, name)
+        if id:
+            p = cls.get_by_id(id)
+            print(f'found by id publisher {p}')
+        elif name:
+            p = cls.get_by_name(name)
+            print(f'found by name publisher {p}')
+        if not p:
+            p = cls(**publisher)
+            print(f'created publisher {p}')
+        return p
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
     def __repr__(self):
         return f"<Publisher({self.name})>"
+
+class TagCategory(PkModel):
+    """Categories which tags can be a part of."""
+    __tablename__ = "tag_categories"
+
+    category = Column(db.String(255))
+
+    def __repr__(self):
+        return f"<TagCategory({self.category})>"
+
+class Tag(PkModel):
+    """Tags to for qualitative attributes."""
+    __tablename__ = "tag"
+
+    tag = Column(db.String(255))
+    category = reference_col("s")
+
+    def __repr__(self):
+        return f"<Tag({self.tag})>"
